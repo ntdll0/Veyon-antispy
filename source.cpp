a81b52256d370ec989ef68ea2db9a5ec0c8d097a
Andrej <164230949+ntdll0@users.noreply.github.com>
2025-02-11 11:27:39 +0100

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,32 +1,17 @@
 #include <Windows.h>
 
-LRESULT CALLBACK proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-	switch (msg) {
-	case WM_DESTROY: {
-		PostQuitMessage(0);
-		return 0;
-	}
-	default: {
-		return DefWindowProc(hwnd, msg, wParam, lParam);
-	}
-	}
-}
-
 int WINAPI WinMain(HINSTANCE hins, HINSTANCE hprev, LPSTR cmd, int show)
 {
 	const wchar_t CLASS[] = L"overlay";
-
 	WNDCLASS wc = {};
-	wc.lpfnWndProc = proc;
+	wc.lpfnWndProc = DefWindowProc;
 	wc.hInstance = hins;
 	wc.lpszClassName = CLASS;
-
 	RegisterClass(&wc);
 
 	HWND hwnd = CreateWindowEx(
 		WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW,
-		CLASS,
-		L"",
+		CLASS, L"",
 		WS_POPUP,
 		0, 0, 
 		GetSystemMetrics(SM_CXSCREEN),
@@ -36,7 +21,6 @@ int WINAPI WinMain(HINSTANCE hins, HINSTANCE hprev, LPSTR cmd, int show)
 
 	SetLayeredWindowAttributes(hwnd, 0, 0, LWA_COLORKEY);
 	SetWindowDisplayAffinity(hwnd, WDA_MONITOR);
-
 	ShowWindow(hwnd, show);
 
 	MSG msg = {};
